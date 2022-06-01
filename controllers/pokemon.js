f53b14c4a6676e6a825adc0879ac3cb92d339717
oyenkachi <calebononobgu@yahoo.com>
2022-06-01 11:56:28 -0700

    first commit

---
+++ controllers/pokemon.js
@@ -0,0 +1,31 @@
+exports.getpokemon = async (req, res) => {
+    const api = await fetch('https://pokeapi.co/api/v2/pokemon/');
+
+    if (api.ok) {
+        const data = await api.json();
+
+        console.log(data);
+
+        try {
+            res.json(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+exports.getPokemonMoves = async (req, res) => {
+    const api = await fetch('https://pokeapi.co/api/v2/move');
+
+    if (api.ok) {
+        const data = await api.json();
+
+        console.log(data);
+
+        try {
+            res.json(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
